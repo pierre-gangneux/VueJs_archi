41536b4af442b89496beccb562ecda3dce4e6bad
baptiste-mignan <baptiste.mignan@gmail.com>
2025-01-22 16:35:13 +0100

    first commit

---
+++ todo/__init__.py
@@ -0,0 +1,3 @@
+from .app import app
+import todo.views
+import todo.models
---
+++ todo/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os.path
+
+app = Flask (__name__)
+
+def mkpath(p):
+    return os.path.normpath(os.path.join(os.path.dirname(__file__), p))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:///'+"myapp.db")
+db = SQLAlchemy(app)
