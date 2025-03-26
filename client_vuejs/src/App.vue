e1bb66574082ec520c548fbbe34b6168907e3206
baptiste-mignan <baptiste.mignan@gmail.com>
2025-03-26 16:41:45 +0100

    get questionnaire et add

--- client_vuejs/src/App.vue
+++ client_vuejs/src/App.vue
@@ -1,30 +1,89 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import questionnaire from './components/Questionnaire.vue';
+
+export default {
+  data() {
+    return {
+      questionnaires: [
+        { id: 1, text: 'Faire les courses', checked: true },
+        { id: 2, text: 'Apprendre REST', checked: false }
+      ],
+      title: 'Mes tâches',
+      newItem: ''
+    };
+  },
+  methods: {
+    addItem() {
+      let name = this.newItem.trim();
+      if (name) {
+        fetch('http://localhost:5000/api/questionnaires',{
+          headers: {'Content-Type': 'application/json'},
+          method: 'POST',
+          body: JSON.stringify({"name":name})
+      })
+      .then(() =>{
+        this.getQuestionnaires();
+      })
+      }
+    },
+    get_questionnaire_by_id: function (id) {
+      for (let i=0; i < this.questionnaires.length; i++){
+        if (this.questionnaires[i].id==id){
+          return this.questionnaires[i];
+        }
+      }
+    },
+    remove ($event) {
+      this.questionnaires = this.questionnaires.filter(questionnaire => questionnaire.id !== $event.id);
+    },
+    edit($event) {
+      let questionnaire = this.questionnaires.find(t => t.id === $event.id);
+      if (questionnaire) {
+        questionnaire.name = $event.name;
+        console.log($event.name)
+      }
+    },
+    getQuestionnaires(){
+      fetch('http://127.0.0.1:5000/api/questionnaires')
+      .then(response => response.json())
+      .then(json=>{
+        console.log(json)
+        this.questionnaires = json
+      })
+    }
+  },
+  mounted() {
+    this.getQuestionnaires();
+    console.log("mounted");
+  },
+  components: { questionnaire }
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <h2>{{ title }}</h2>
+    <ol>
+      <questionnaire 
+        v-for="questionnaire in questionnaires"
+        :key="questionnaire.id"
+        :questionnaire="questionnaire"
+        :class="{ 'alert alert-success': questionnaire.checked }"
+        @remove="remove"
+        @edit="edit"
+      >
+        {{ questionnaire.name }}
+      </questionnaire>
+    </ol>
+    <div class="input-group">
+      <input
+        v-model="newItem"
+        @keyup.enter="addItem"
+        placeholder="Ajouter une tâche"
+        type="text"
+        class="form-control"
+      />
+      <button @click="addItem" class="btn btn-primary">Ajouter</button>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
